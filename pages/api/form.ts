6d335dfaa5c921448acaaf8e558d27f01672a3d7
Solomon Ayoola <solomon.ayoola@matchday.com>
2023-05-01 23:40:54 +0200

    refactor: edit form.ts api file

--- pages/api/form.ts
+++ pages/api/form.ts
@@ -40,48 +40,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   <head>
     <title>Frospices Newsletter Signup Confirmation</title>
     <style>
-      body {
+      p {
         font-family: Arial, sans-serif;
+        color: #333;
         font-size: 14px;
         line-height: 1.6;
-        color: #333;
-      }
-
-      h1 {
-        font-size: 24px;
-        font-weight: 700;
-        margin-bottom: 30px;
-      }
-
-      p {
+        max-width: 600px;
+        margin: 0 auto;
         margin-bottom: 20px;
-      }
-
-      .button {
-        display: inline-block;
-        padding: 10px 20px;
-        font-size: 16px;
-        font-weight: 700;
-        text-align: center;
-        color: #fff;
-        background-color: #2f5be8;
-        border-radius: 4px;
-        text-decoration: none;
-      }
-
-      .button:hover {
-        background-color: #1a3da8;
       }
 
       .logo {
         display: block;
         margin: 0 auto;
         max-width: 200px;
+        margin-bottom: 20px;
+      }
+
+      #team-name {
+        margin-bottom: 0;
       }
     </style>
   </head>
   <body>
-    <img src="https://www.frospices.com/logo.svg" alt="Frospices Logo" class="logo">
+    <img src="https://www.frospices.com/logo.png" alt="Frospices Logo" class="logo">
     <p>Thank you for subscribing to the Frospices newsletter! We're excited to have you on board, and we can't wait to share our latest news and updates with you.</p>
 
 <p>As a subscriber, you'll receive regular emails from us with exclusive recipes, news about upcoming events and promotions, and much more. We'll keep you informed on everything happening at Frospices.</p>
@@ -90,7 +72,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
 <p>Thanks again for joining us, and we look forward to keeping in touch!</p>
 
-<p>Best regards,</p>
+<p id="team-name">Best regards,</p>
 <p>The Frospices Team</p> 
   </body>
 </html>
